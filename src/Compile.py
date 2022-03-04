f71013cbcee76a37f5a87bc6548a498ef17a7bf6
csekri <csehkristof@gmail.com>
2022-03-04 15:36:03 +0000

    os.system replaced by QProcess when running system commands

--- src/Compile.py
+++ src/Compile.py
@@ -1,9 +1,15 @@
-from os import system
+from PyQt5.QtCore import QProcess
+
 
 def compile_latex(scene, auto_compile_check):
     if (not auto_compile_check) or (auto_compile_check and scene.auto_compile):
         print(scene.project_data.doc_surround_tikzify(*scene.current_canvas_dims, *scene.init_canvas_dims))
         with open('try.tex', 'w') as f:
             f.write(scene.project_data.doc_surround_tikzify(*scene.current_canvas_dims, *scene.init_canvas_dims))
-        system(f'pdflatex -synctex=1 -interaction=batchmode --shell-escape -halt-on-error try.tex')
-        system(f'pdftocairo -png -scale-to-x {scene.current_canvas_dims[0]} -scale-to-y {scene.current_canvas_dims[1]} try.pdf')
+        pdf_command = r'pdflatex -synctex=1 -interaction=batchmode --shell-escape -halt-on-error try.tex'
+        jpg_command = f'pdftocairo -png -scale-to-x {scene.current_canvas_dims[0]} -scale-to-y {scene.current_canvas_dims[1]} try.pdf'
+        process = QProcess()
+        process.start(pdf_command)
+        process.waitForFinished()
+        process.start(jpg_command)
+        process.waitForFinished()
